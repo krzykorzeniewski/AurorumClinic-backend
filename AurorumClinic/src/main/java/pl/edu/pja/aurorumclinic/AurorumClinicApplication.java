bca7acb579ab6f5dde4cdf1301d572bca2f19822
k0rz3n <k0rz3n@pm.me>
2025-06-18 14:42:25 +0200

    chore: init

---
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/AurorumClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.aurorumclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurorumClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AurorumClinicApplication.class, args);
+    }
+
+}
---
+++ AurorumClinic/src/test/java/pl/edu/pja/aurorumclinic/AurorumClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.aurorumclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurorumClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
