355e4c6647e55ebeb9b25bf10d888fafd8d5ea79
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-07-24 13:42:35 +0200

    feat: change cors config

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/security/SecurityConfig.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/security/SecurityConfig.java
@@ -91,6 +91,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(clientUrl));
         configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
