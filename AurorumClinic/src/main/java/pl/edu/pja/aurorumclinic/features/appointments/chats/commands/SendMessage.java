b5b6f2b6f12fbdb63be510abf3fe1b9c504af441
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-10-17 18:06:48 +0200

    refactor: move send message dto to endpoint class

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/chats/commands/SendMessage.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/chats/commands/SendMessage.java
@@ -1,12 +1,14 @@
 package pl.edu.pja.aurorumclinic.features.appointments.chats.commands;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
-import pl.edu.pja.aurorumclinic.features.appointments.chats.MessageDto;
 import pl.edu.pja.aurorumclinic.features.appointments.chats.MessageRepository;
 import pl.edu.pja.aurorumclinic.shared.data.UserRepository;
 import pl.edu.pja.aurorumclinic.shared.data.models.Message;
@@ -14,6 +16,7 @@ import pl.edu.pja.aurorumclinic.shared.data.models.User;
 import pl.edu.pja.aurorumclinic.shared.exceptions.ApiNotFoundException;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,7 +28,7 @@ public class SendMessage {
 
     @MessageMapping("/chat")
     @Transactional
-    public void handle(@Valid MessageDto message,
+    public void handle(@Valid SendMessageRequest message,
                              Principal principal) {
         Long senderId = Long.valueOf(principal.getName());
         User sender = userRepository.findById(senderId).orElseThrow(
@@ -42,7 +45,13 @@ public class SendMessage {
                 .build();
         messageRepository.save(newMessage);
         simpMessagingTemplate.convertAndSendToUser(String.valueOf(receiver.getId()),
-                "/queue/messages", message);
+                "/queue/messages", message.text);
     }
+
+    public record SendMessageRequest(@NotEmpty @Size(max = 500) String text,
+                                     @NotNull LocalDateTime sentAt,
+                                     @NotNull Long receiverId) {
+    }
+
 
 }
