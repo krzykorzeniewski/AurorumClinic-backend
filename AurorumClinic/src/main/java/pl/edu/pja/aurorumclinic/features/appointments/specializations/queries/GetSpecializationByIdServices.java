b52c9eced5e6143469bfc21eedc132c04cd25e87
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-10-19 20:38:31 +0200

    feat: add price to spec services response

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/specializations/queries/GetSpecializationByIdServices.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/specializations/queries/GetSpecializationByIdServices.java
@@ -1,5 +1,6 @@
 package pl.edu.pja.aurorumclinic.features.appointments.specializations.queries;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.annotation.security.PermitAll;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,8 @@ import pl.edu.pja.aurorumclinic.shared.ApiResponse;
 import pl.edu.pja.aurorumclinic.shared.data.SpecializationRepository;
 import pl.edu.pja.aurorumclinic.shared.data.models.Service;
 import pl.edu.pja.aurorumclinic.shared.exceptions.ApiNotFoundException;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/specializations")
@@ -40,12 +43,14 @@ public class GetSpecializationByIdServices {
         return services.map(service -> GetSpecByIdServicesResponse.builder()
                 .id(service.getId())
                 .name(service.getName())
+                .price(service.getPrice())
                 .build());
     }
 
     @Builder
     public record GetSpecByIdServicesResponse(Long id,
-                                              String name) {
+                                              String name,
+                                              @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT) BigDecimal price) {
 
     }
 
