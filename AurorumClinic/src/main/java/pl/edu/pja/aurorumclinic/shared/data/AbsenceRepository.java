14939c81dfd87d1db9537360eaa4de7f2c5c3c92
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-11-12 20:48:40 +0100

    feat: all absences and all absences by doctor id endpoints for emp

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/shared/data/AbsenceRepository.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/shared/data/AbsenceRepository.java
@@ -1,10 +1,14 @@
 package pl.edu.pja.aurorumclinic.shared.data;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import pl.edu.pja.aurorumclinic.features.users.doctors.queries.GetDoctorByIdAbsences;
 import pl.edu.pja.aurorumclinic.shared.data.models.Absence;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface AbsenceRepository extends JpaRepository<Absence, Long> {
 
@@ -27,4 +31,20 @@ public interface AbsenceRepository extends JpaRepository<Absence, Long> {
             """)
     boolean absenceExistsInIntervalForDoctorExcludingId(LocalDateTime startedAt, LocalDateTime finishedAt, Long doctorId,
                                                         Long absenceId);
+
+    @Query("""
+            select a
+                from Absence a
+                where a.startedAt >= :startedAt
+                  and a.finishedAt <= :finishedAt
+            """)
+    Page<Absence> findAllBetween(LocalDateTime startedAt, LocalDateTime finishedAt, Pageable pageable);
+
+    @Query("""
+            select a from Absence a
+                  where a.doctor.id = :doctorId
+                  and a.startedAt < :finishedAt
+                  and a.finishedAt > :startedAt
+            """)
+    List<Absence> findAllByDoctorIdAndBetween(Long doctorId, LocalDateTime startedAt, LocalDateTime finishedAt);
 }
