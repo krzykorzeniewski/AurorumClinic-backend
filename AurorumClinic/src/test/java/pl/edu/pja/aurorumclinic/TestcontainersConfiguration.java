fed88d09a4e976ef387277307146dfffb606b87a
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-11-18 23:27:10 +0100

    feat: working testcontainers integration

---
+++ AurorumClinic/src/test/java/pl/edu/pja/aurorumclinic/TestcontainersConfiguration.java
@@ -0,0 +1,30 @@
+package pl.edu.pja.aurorumclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    MSSQLServerContainer<?> sqlServerContainer() {
+        return new MSSQLServerContainer<>(DockerImageName
+                .parse("mcr.microsoft.com/mssql/server:2022-latest"));
+    }
+
+
+}
+
+
