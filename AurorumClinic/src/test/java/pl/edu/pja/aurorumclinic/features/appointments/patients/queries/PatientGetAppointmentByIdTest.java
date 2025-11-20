89ba48aa4573b1f4f4b04ac138422c90582aa1b5
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-11-20 16:58:05 +0100

    feat: patient queries unit tests

---
+++ AurorumClinic/src/test/java/pl/edu/pja/aurorumclinic/features/appointments/patients/queries/PatientGetAppointmentByIdTest.java
@@ -0,0 +1,146 @@
+package pl.edu.pja.aurorumclinic.features.appointments.patients.queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pl.edu.pja.aurorumclinic.features.appointments.patients.queries.shared.PatientGetAppointmentResponse;
+import pl.edu.pja.aurorumclinic.shared.ApiResponse;
+import pl.edu.pja.aurorumclinic.shared.data.AppointmentRepository;
+import pl.edu.pja.aurorumclinic.shared.data.models.*;
+import pl.edu.pja.aurorumclinic.shared.data.models.enums.AppointmentStatus;
+import pl.edu.pja.aurorumclinic.shared.data.models.enums.PaymentStatus;
+import pl.edu.pja.aurorumclinic.shared.exceptions.ApiNotFoundException;
+import pl.edu.pja.aurorumclinic.shared.services.ObjectStorageService;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {PatientGetAppointmentById.class})
+@ActiveProfiles("test")
+public class PatientGetAppointmentByIdTest {
+
+    @MockitoBean
+    AppointmentRepository appointmentRepository;
+
+    @MockitoBean
+    ObjectStorageService objectStorageService;
+
+    @Autowired
+    PatientGetAppointmentById patientGetAppointmentById;
+
+    @Test
+    void shouldThrowApiNotFoundWhenAppointmentIdIsNotFound() {
+        Patient testPatient = Patient.builder()
+                .id(1L)
+                .build();
+        Appointment testAppointment = Appointment.builder()
+                .id(1L)
+                .status(AppointmentStatus.FINISHED)
+                .startedAt(LocalDateTime.now().minusMinutes(30))
+                .description("Lęki")
+                .doctor(Doctor.builder()
+                        .id(1L)
+                        .name("Mariusz")
+                        .surname("Mariuszowski")
+                        .profilePicture("example.pmg")
+                        .specializations(Set.of(Specialization.builder()
+                                .id(1L)
+                                .name("Psychiatra dorosłych")
+                                .build()))
+                        .build())
+                .service(Service.builder()
+                        .id(1L)
+                        .name("Konsultacja psychiatryczna (kolejna wizyta)")
+                        .price(BigDecimal.valueOf(10000))
+                        .build())
+                .payment(Payment.builder()
+                        .id(1L)
+                        .amount(BigDecimal.valueOf(10000))
+                        .status(PaymentStatus.CREATED)
+                        .build())
+                .patient(testPatient)
+                .build();
+
+        when(appointmentRepository.getAppointmentByIdAndPatientId(anyLong(), anyLong())).thenReturn(null);
+
+        assertThatThrownBy(
+                () -> patientGetAppointmentById.getAppointment(testAppointment.getId(), testPatient.getId())
+        ).isExactlyInstanceOf(ApiNotFoundException.class);
+        verify(appointmentRepository).getAppointmentByIdAndPatientId(testAppointment.getId(), testPatient.getId());
+    }
+
+    @Test
+    void shouldReturnDtoWithCorrectAppointmentFieldsForPatientId() {
+        Patient testPatient = Patient.builder()
+                .id(1L)
+                .build();
+        Appointment testAppointment = Appointment.builder()
+                .id(1L)
+                .status(AppointmentStatus.FINISHED)
+                .startedAt(LocalDateTime.now().minusMinutes(30))
+                .description("Lęki")
+                .doctor(Doctor.builder()
+                        .id(1L)
+                        .name("Mariusz")
+                        .surname("Mariuszowski")
+                        .profilePicture("example.pmg")
+                        .specializations(Set.of(Specialization.builder()
+                                .id(1L)
+                                .name("Psychiatra dorosłych")
+                                .build()))
+                        .build())
+                .service(Service.builder()
+                        .id(1L)
+                        .name("Konsultacja psychiatryczna (kolejna wizyta)")
+                        .price(BigDecimal.valueOf(10000))
+                        .build())
+                .payment(Payment.builder()
+                        .id(1L)
+                        .amount(BigDecimal.valueOf(10000))
+                        .status(PaymentStatus.CREATED)
+                        .build())
+                .patient(testPatient)
+                .build();
+        String imageUrl = "some image url";
+
+        when(appointmentRepository.getAppointmentByIdAndPatientId(anyLong(), anyLong())).thenReturn(testAppointment);
+        when(objectStorageService.generateUrl(anyString())).thenReturn(imageUrl);
+
+        ResponseEntity<ApiResponse<PatientGetAppointmentResponse>> resultResponse =
+                patientGetAppointmentById.getAppointment(testAppointment.getId(), testPatient.getId());
+
+        assertThat(resultResponse.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(resultResponse.getBody()).isNotNull();
+
+        PatientGetAppointmentResponse resultDto = resultResponse.getBody().getData();
+        assertThat(resultDto).isNotNull();
+        assertThat(resultDto.id()).isEqualTo(testAppointment.getId());
+        assertThat(resultDto.status()).isEqualTo(testAppointment.getStatus());
+        assertThat(resultDto.startedAt()).isEqualTo(testAppointment.getStartedAt());
+
+        assertThat(resultDto.doctor().id()).isEqualTo(testAppointment.getDoctor().getId());
+        assertThat(resultDto.doctor().name()).isEqualTo(testAppointment.getDoctor().getName());
+        assertThat(resultDto.doctor().specializations()).hasSize(1);
+        assertThat(resultDto.doctor().profilePicture()).isEqualTo(imageUrl);
+
+        assertThat(resultDto.service().id()).isEqualTo(testAppointment.getService().getId());
+        assertThat(resultDto.service().name()).isEqualTo(testAppointment.getService().getName());
+
+        assertThat(resultDto.payment().id()).isEqualTo(testAppointment.getPayment().getId());
+        assertThat(resultDto.payment().status()).isEqualTo(testAppointment.getPayment().getStatus());
+
+        verify(appointmentRepository).getAppointmentByIdAndPatientId(testAppointment.getId(), testPatient.getId());
+        verify(objectStorageService).generateUrl(testAppointment.getDoctor().getProfilePicture());
+    }
+}
