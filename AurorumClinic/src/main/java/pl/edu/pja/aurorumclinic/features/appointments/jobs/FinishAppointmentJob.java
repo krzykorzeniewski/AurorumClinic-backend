1412ade1c565233655783350d600a0637b90ab9d
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2025-11-20 20:18:38 +0100

    feat: patient delete and create appointment unit tests

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/jobs/FinishAppointmentJob.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/jobs/FinishAppointmentJob.java
@@ -1,10 +1,8 @@
 package pl.edu.pja.aurorumclinic.features.appointments.jobs;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import pl.edu.pja.aurorumclinic.features.appointments.shared.events.AppointmentFinishedEvent;
 import pl.edu.pja.aurorumclinic.shared.data.AppointmentRepository;
 import pl.edu.pja.aurorumclinic.shared.data.models.Appointment;
 import pl.edu.pja.aurorumclinic.shared.data.models.enums.AppointmentStatus;
@@ -15,7 +13,6 @@ import pl.edu.pja.aurorumclinic.shared.exceptions.ApiNotFoundException;
 public class FinishAppointmentJob {
 
     private final AppointmentRepository appointmentRepository;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
     public void execute(Long appointmentId) {
@@ -23,7 +20,6 @@ public class FinishAppointmentJob {
                 () -> new ApiNotFoundException("Id not found", "id")
         );
         appointmentFromDb.setStatus(AppointmentStatus.FINISHED);
-        applicationEventPublisher.publishEvent(new AppointmentFinishedEvent(appointmentFromDb));
     }
 
 }
