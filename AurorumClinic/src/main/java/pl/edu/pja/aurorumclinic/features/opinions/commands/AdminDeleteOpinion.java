3366c6d03672c1512c0b0f10378b0c07d4e773ad
kawhiki <s27609@pjwstk.edu.pl>
2026-01-06 19:44:36 +0100

    feat: admin delete opinion

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/opinions/commands/AdminDeleteOpinion.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/opinions/commands/AdminDeleteOpinion.java
@@ -3,7 +3,6 @@ package pl.edu.pja.aurorumclinic.features.opinions.commands;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,33 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pja.aurorumclinic.shared.ApiResponse;
 import pl.edu.pja.aurorumclinic.shared.data.OpinionRepository;
 import pl.edu.pja.aurorumclinic.shared.data.models.Opinion;
-import pl.edu.pja.aurorumclinic.shared.exceptions.ApiAuthorizationException;
 import pl.edu.pja.aurorumclinic.shared.exceptions.ApiNotFoundException;
 
 @RestController
-@RequestMapping("/api/doctors/me/opinions")
+@RequestMapping("/api/admin/me/opinions")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('DOCTOR')")
-public class DoctorDeleteOpinion {
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminDeleteOpinion {
 
     private final OpinionRepository opinionRepository;
 
     @DeleteMapping("/{opinionId}")
     @Transactional
-    public ResponseEntity<ApiResponse<String>> delete(
-            @AuthenticationPrincipal Long doctorId,
-            @PathVariable Long opinionId
-    ) {
-        return ResponseEntity.ok(ApiResponse.success(handle(doctorId, opinionId)));
+    public ResponseEntity<ApiResponse<String>> delete(@PathVariable Long opinionId) {
+        return ResponseEntity.ok(ApiResponse.success(handle(opinionId)));
     }
 
-    private String handle(Long doctorId, Long opinionId) {
+    private String handle(Long opinionId) {
         Opinion op = opinionRepository.findById(opinionId)
                 .orElseThrow(() -> new ApiNotFoundException("Opinion not found", "opinionId"));
-
-        if (!op.getAppointment().getDoctor().getId().equals(doctorId)) {
-            throw new ApiAuthorizationException("You cannot delete someone else's opinion");
-        }
 
         opinionRepository.delete(op);
         return "Opinion deleted successfully";
