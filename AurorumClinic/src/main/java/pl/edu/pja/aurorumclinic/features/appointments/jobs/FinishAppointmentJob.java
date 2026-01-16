1b311b38860f15d7ab0b99572a8ff5264a2491bc
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2026-01-16 18:35:51 +0100

    chore: add log to appointment finish job

--- AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/jobs/FinishAppointmentJob.java
+++ AurorumClinic/src/main/java/pl/edu/pja/aurorumclinic/features/appointments/jobs/FinishAppointmentJob.java
@@ -1,6 +1,7 @@
 package pl.edu.pja.aurorumclinic.features.appointments.jobs;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import pl.edu.pja.aurorumclinic.shared.data.AppointmentRepository;
@@ -10,16 +11,22 @@ import pl.edu.pja.aurorumclinic.shared.exceptions.ApiNotFoundException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FinishAppointmentJob {
 
     private final AppointmentRepository appointmentRepository;
 
     @Transactional
     public void execute(Long appointmentId) {
-        Appointment appointmentFromDb = appointmentRepository.findById(appointmentId).orElseThrow(
-                () -> new ApiNotFoundException("Id not found", "id")
-        );
-        appointmentFromDb.setStatus(AppointmentStatus.FINISHED);
+        try {
+            Appointment appointmentFromDb = appointmentRepository.findById(appointmentId).orElseThrow(
+                    () -> new ApiNotFoundException("Id not found", "id")
+            );
+            appointmentFromDb.setStatus(AppointmentStatus.FINISHED);
+        } catch (Exception e) {
+            log.error("Error executing FinishAppointmentJob for appointmentId: {}", appointmentId, e);
+            throw e;
+        }
     }
 
 }
